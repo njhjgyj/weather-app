55ce4303337a7738219de4f98af92162299946e5
Boost <90213458+njhjgyj@users.noreply.github.com>
2023-08-05 15:42:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+// var API_KEY = "c56a2006fe9ded964db1f076a7bc7ce0";
+// var city = 'karachi';
+// var countryCode = '+92';
+// var stateCode = 'PK-SD';
+// var API = `https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},${countryCode}&appid=${API_KEY}`;
+
+// fetch(API)
+//     .then(response => response.json())
+//     .then(data => {
+
+//         console.log(data);
+
+//     })
+//     .catch(error => {
+
+//         console.error("Error fetching data: ", error);
+
+//     });
+
+
+
+
+
+
+var formId = document.getElementById('form-id1');
+
+var inpId = document.getElementById('inp-id1-for-input');
+
+var searchInpId = document.getElementById('inp-id2-for-search');
+
+var locationEnterByUser = document.getElementById('h2-id2-for-location-enter-by-user');
+
+var weatherQuality = document.getElementById('b-id1-for-weather-quality');
+
+var temprature = document.getElementById('p-id1-for-temprature');
+
+var minTemp = document.getElementById('b-id2-for-mintemp');
+var minTempValue = document.getElementById('b-id3-for-mintemp-value');
+
+var maxTemp = document.getElementById('b-id4-for-maxtemp');
+var maxTempValue = document.getElementById('b-id5-for-maxtemp-value');
+
+
+searchInpId.addEventListener('click', clickFunction);
+
+function clickFunction() {
+
+
+    if (inpId.value == "") {
+
+        alert("Please fill out the input field");
+        inpId.focus();
+
+    } else {
+
+
+        var API_KEY = "c56a2006fe9ded964db1f076a7bc7ce0";
+        var city = inpId.value;
+        console.log(city);
+        var countryCode = '+92';
+        var API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+        fetch(API)
+            .then(response => response.json())
+            .then(data => {
+
+                console.log(data);
+
+                if (data.cod == '404') {
+
+                    locationEnterByUser.innerText = data.cod + ' ' + data.message;
+
+                } else {
+
+                    if (data.sys.country == null) {
+
+                        locationEnterByUser.innerText = data.name;
+
+                    } else {
+
+                        locationEnterByUser.innerText = data.sys.country + ' ' + data.name;
+
+                    };
+
+                };
+
+
+                weatherQuality.innerText = data.weather[0].description;
+                temprature.innerText = data.main.temp;
+
+                minTempValue.innerText = data.main.temp_min;
+                maxTempValue.innerText = data.main.temp_max;
+
+
+            })
+            .catch(error => {
+
+                console.error("Error fetching data: ", error);
+
+            });
+
+
+        // locationEnterByUser.innerText = inpId.value;
+        inpId.value = "";
+
+    };
+
+};
